0ac3cc30db5f9b191c4c758628f542f4eef5a33d
valerijivlev <qa@>
2024-09-08 11:17:19 +0300

    123

--- src/com/example/storage/ArrayStorage.java
+++ src/com/example/storage/ArrayStorage.java
@@ -1,38 +1,45 @@
+package com.example.storage;
+
+import com.example.model.Resume;
+
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
-        storage[size] = r;
-        size++;
+    public void save(Resume r) {
+            storage[size] = r;
+            size++;
     }
 
-    Resume get(String uuid) {
+    public void update(Resume r){
+
+    }
+
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if(storage[i].uuid.equals(uuid)){
+            if(storage[i].getUuid().equals(uuid)){
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
@@ -44,11 +51,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.stream(storage).limit(size).toArray(Resume[]::new);
     }
 
-    int size(){
+    public int size(){
         return size;
     }
 }
